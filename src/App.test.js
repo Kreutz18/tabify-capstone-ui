ab72fa7089d31ed93d4cc076751cfaeb4ed347a5
Kreutz18 <32467053+Kreutz18@users.noreply.github.com>
2023-01-27 20:24:12 -0500

    PSC-13 update tab title to Tabify

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// text to verify link exists in DOM
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
