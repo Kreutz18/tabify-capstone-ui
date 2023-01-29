746f59d376728e600bb0082ca4683acaa0aada29
Kreutz18 <32467053+Kreutz18@users.noreply.github.com>
2023-01-28 22:50:50 -0500

    PSC-16 added home screen

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
-import { BandView } from "./band-view/band-view";
-import { Playlists } from "./playlists/playlists";
-import { Login } from "./login/login";
+import { Home } from "./home/home.js";
+import { BandView } from "./band-view/band-view.js";
+import { Playlists } from "./playlists/playlists.js";
+import { Login } from "./login/login.js";
 
 export function AppRouter() {
   return (
     <Routes>
+      <Route path="/home" element={<Home />} />
       <Route path="/band" element={<BandView />} />
       <Route path="/playlists" element={<Playlists />} />
       <Route path="/login" element={<Login />} />
