15701a9b04987b7e0086955f3c3ca69e489ed1ce
Jesse <JesseKKreutzberger@gmail.com>
2023-02-17 13:21:56 -0500

    added login

--- src/login/login.js
+++ src/login/login.js
@@ -1,5 +1,11 @@
 export function Login() {
-  return (
-    <p>Login Works!</p>
-  )
+  return getSpotifyUserLogin();
+}
+
+function getSpotifyUserLogin() {
+  fetch("http://localhost:8080/api/login")
+  .then((response) => response.text())
+  .then(response => {
+    window.location.replace(response);
+  })
 }
