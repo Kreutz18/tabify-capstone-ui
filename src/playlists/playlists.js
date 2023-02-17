8e56dd2a660febdf1dd57b32a0fb77fd7feb6a00
Jesse <JesseKKreutzberger@gmail.com>
2023-02-17 13:25:16 -0500

    added playlist function

--- src/playlists/playlists.js
+++ src/playlists/playlists.js
@@ -2,10 +2,18 @@ import { SlidePanel } from '../slide-panel/slide-panel';
 import './playlists.css';
 
 export function Playlists() {
+  const playlists = getUsersPlaylists();
   return (
     <>
       <p>Playlists Works!</p>
       <SlidePanel />
     </>
   )
+}
+
+function getUsersPlaylists() {
+  fetch("http://localhost:8080/api/user-playlists")
+    .then((response) => {
+      console.log(response);
+    }).catch((e) => {console.log(e);})
 }
