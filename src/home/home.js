6b4d1ff136099e906bfcb3946a87e735535bb1de
kingkaungkhine1 <61713494+kingkaungkhine1@users.noreply.github.com>
2023-03-03 12:35:57 -0500

    Log in features

--- src/home/home.js
+++ src/home/home.js
@@ -34,3 +34,29 @@ const getReturnedParamsFromSpotifyAuth = (hash) => {
   return paramsSplitUp;
 };
 
+function getUserProfile() 
+{
+  const accessToken = localStorage.getItem("accessToken");
+  console.log(accessToken);
+  fetch("https://api.spotify.com/v1/me", {
+      method: 'get',
+      headers: {
+        'Accept': 'application/json',
+        'Content-type': 'application/json',
+        'Authorization': 'Bearer ' + accessToken
+      }
+  })
+    .then((response) => response.json())
+    .then((data) => localStorage.setItem("profile", JSON.stringify(data)))
+    .then((data) => console.log(JSON.stringify(data)));
+
+
+  var profile = localStorage.getItem("profile");
+  console.log(profile);
+
+  var parseTest = JSON.parse(profile);
+  var name = parseTest.display_name;
+
+  console.log(name);
+  return name;
+}
