c20889aea6487842a89133e12dfc7cd03f1dd6b4
Jesse <JesseKKreutzberger@gmail.com>
2023-03-02 20:58:25 -0500

    psc-36 fixed up playlists and added delete dropdown

--- src/Spinner.js
+++
@@ -1,3 +0,0 @@
-export function Spinner() {
-  return <Spinner animation="border" variant="dark"></Spinner>
-}
--- src/playlists/playlists.js
+++ src/playlists/playlists.js
@@ -1,46 +1,32 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useEffect, useState } from 'react';
-import { Button, Col, Container, Row, Table } from 'react-bootstrap';
+import { React, useEffect, useState } from 'react';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 import { SlidePanel } from '../slide-panel/slide-panel';
+import { PlaylistTable } from './playlist-table/PlaylistTable';
+import { LoadingSpinner } from '../LoadingSpinner';
 import './playlists.scss';
 
-const hardCodedPlaylists = [
-  {id: 1, name: 'Rock This'},
-  {id: 2, name: 'Country Mix'},
-  {id: 3, name: 'Luke Combs Radio'},
-  {id: 4, name: 'CCMF 2021'},
-  {id: 5, name: 'Blues'},
-  {id: 6, name: 'Jazz'},
-  {id: 7, name: 'Tunes'},
-  {id: 8, name: 'Oscars'},
-  {id: 9, name: 'Rap'},
-  {id: 10, name: 'Outlaw Country'},
-  {id: 11, name: 'Good Stuff'},
-  {id: 12, name: 'Liked'},
-  {id: 13, name: 'Good Vibes'},
-  {id: 14, name: 'Starred'},
-  {id: 15, name: 'Band'},
-  {id: 16, name: 'SkatePark Punks'},
-];
+const headers = {
+  headers: new Headers({ 'Authorization': `Bearer ${localStorage.getItem("accessToken")}` }),
+  method: 'get'
+};
 
 export function Playlists() {
+  const addSongsMessage = 'Add songs to view playlist';
+  const errorMessage = 'Error while fetching playlist';
   const [playlists, setPlaylists] = useState([]);
   const [previousTarget, setPreviousTarget] = useState(null);
   const [playlistTracks, setPlaylistTracks] = useState([]);
   const [playlistSelected, setPlaylistSelected] = useState(false);
   const [hasSongs, setHasSongs] = useState(false);
-
-  const headers = {
-    method: 'get',
-    headers: new Headers({ 'Authorization': `Bearer ${localStorage.getItem("accessToken")}` })
-  };
+  const [isLoading, setIsLoading] = useState(false);
+  const [playlistMessage, setPlaylistMessage] = useState(addSongsMessage);
 
   useEffect(() => {
     fetch("https://api.spotify.com/v1/me/playlists", headers).then(response => response.json()).then((data) => {
       setPlaylists(data.items);
     });
   }, []);
-
+  
   function selectItem(event, item) {
     if (previousTarget) {
       if (previousTarget !== event.target) {
@@ -54,23 +40,22 @@ export function Playlists() {
       event.target.classList.add('selected');
       setPreviousTarget(event.target);
     }
-
+    
     fetchTracks(item.tracks.href);
   }
-
+  
   function fetchTracks(trackUrl) {
+    setIsLoading(true);
     fetch(trackUrl + '?offset=0&limit=20', headers).then(response => response.json()).then((tracks) => {
       setPlaylistTracks(tracks);
       setPlaylistSelected(true);
-      setHasSongs(tracks.items.length > 0 ? true : false);
+      setHasSongs(tracks.items && tracks.items.length > 0 ? true : false);
+      setPlaylistMessage(addSongsMessage);
+      setIsLoading(false);
+    }, err => {
+      setIsLoading(false);
+      setPlaylistMessage(errorMessage);
     });
-  }
-
-  function millisToMinutesAndSeconds(milliseconds) {
-    const seconds = Math.floor((milliseconds / 1000) % 60);
-    const minutes = Math.floor((milliseconds / 1000 / 60) % 60);
-    const hours = Math.floor((milliseconds / 1000 / 60 / 60) % 24);
-    return (hours ? hours + ":" : '') + (hours && minutes < 10 ? '0' : '') + minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
   }
 
   return (
@@ -90,38 +75,13 @@ export function Playlists() {
           </Col>
           <Col>
           {playlistSelected ? (
-            hasSongs ? (
-              <Table hover responsive>
-                <thead>
-                  <tr className='align-left'>
-                    <th>#</th>
-                    <th>Title</th>
-                    <th>Album</th>
-                    <th>Date Added</th>
-                    <th><FontAwesomeIcon icon="fa-regular fa-clock" /></th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {playlistTracks.items?.map((item, i) => {
-                    return (
-                      <tr key={i} className='align-left'>
-                        <td key={'number-' + item.track.id}>{i + 1}</td>
-                        <td key={'name-' + item.track.id}>{item.track.name}</td>
-                        <td key={item.track.album.id + '-' + item.track.id}>{item.track.album.name}</td>
-                        <td key={'time-' + item.track.id + '-' + item.track.id}>{millisToMinutesAndSeconds(item.track.duration_ms)}</td>
-                        <td key={'chevron-' + item.track.id} className='clickable'><FontAwesomeIcon icon="fa-solid fa-chevron-down" /></td>
-                      </tr>
-                    )
-                  })}
-                </tbody>
-              </Table>
-            ) : (
-              <p>Add songs to view playlist</p>
-            )
-          ) : 
-          (
-            <p>Select a playlist</p>
-          )}
+            isLoading ? (
+              <LoadingSpinner />
+            ) : (hasSongs ? (
+                <PlaylistTable playlistTracks={playlistTracks}/>
+              ) : (<p>Add songs to view playlist</p>))
+            ) : (<p>{playlistMessage}</p>)
+          }
           </Col>
           <Col sm={1}>
             <SlidePanel />
